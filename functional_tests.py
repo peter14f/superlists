f12caf6c37580ed538288f407a52ce4c932c7239
Peter Hsieh <peter14f@gmail.com>
2015-06-10 18:04:56 +0800

    Functional test now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -13,8 +13,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        #self.browser.get('http://peter14f.pythonanywhere.com')
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://peter14f.pythonanywhere.com')
+        #self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)

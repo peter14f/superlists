531f68f593cd16b16412bdadc32bb3c223feeb91
Peter Hsieh <peter14f@gmail.com>
2015-06-28 17:16:08 +0800

    Refactor list view to handle new item POSTs.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,27 +8,6 @@ from lists.views import home_page
 from lists.models import Item, List
 
 # Create your tests here.
-class NewItemTest(TestCase):
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        correct_list = List.objects.create()
-        self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data={'item_text': 'A new item for an existing list'}
-        )
-        self.assertEqual(1, Item.objects.count())
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        correct_list = List.objects.create()
-        response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            {'item_text': 'A new item for an existing list'}
-        )
-        new_item = Item.objects.first()
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,), )
-
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
@@ -92,6 +71,29 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        correct_list = List.objects.create()
+        self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertEqual(1, Item.objects.count())
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            {'item_text': 'A new item for an existing list'}
+        )
+        new_item = Item.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,), )
+
 
 class HomePageTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -7,11 +7,6 @@ from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 # Create your views here.
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
 def new_list(request):
     list_ = List.objects.create()
     item_text = request.POST['item_text']
@@ -29,7 +24,9 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
 def home_page(request):

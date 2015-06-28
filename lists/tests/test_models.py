d41dbc0274b20185ddbb65c112f7504087dc2aab
Peter Hsieh <peter14f@gmail.com>
2015-06-28 16:11:57 +0800

    Adjust new list view to do model validation.

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,9 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
 # Create your tests here.
 class ListAndItemModelTest(TestCase):
-
 
     def test_saving_and_retrieving_items(self):
         list_ = List()
@@ -31,3 +31,10 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -14,7 +15,16 @@ def add_item(request, list_id):
 def new_list(request):
     list_ = List.objects.create()
     item_text = request.POST['item_text']
-    Item.objects.create(text=item_text, list=list_)
+    item = Item(text=item_text, list=list_)
+
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+
     return redirect('/lists/%d/' % (list_.id,))
 
 def view_list(request, list_id):
